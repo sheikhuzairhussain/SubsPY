e13e05b5b11a53f591966a20c4da722a5044243b
Sheikh Uzair Hussain <uzair.hussain10@gmail.com>
2020-05-26 22:53:04 +0530

    Bug fixes and metadata feature.

--- SubsPY.py
+++ SubsPY.py
@@ -1,12 +1,7 @@
-import os, hashlib, requests
-def get_hash(name):
-	readsize = 64 * 1024
-	with open(name, 'rb') as f:
-		size = os.path.getsize(name)
-		data = f.read(readsize)
-		f.seek(-readsize, os.SEEK_END)
-		data += f.read(readsize)
-	return hashlib.md5(data).hexdigest()
+import os
+from pythonopensubtitles.opensubtitles import OpenSubtitles
+from pythonopensubtitles.utils import File
+from pathlib import Path
 
 def get_path():
 	path = ""
@@ -20,36 +15,32 @@ def get_path():
 
 def main():
 	path = get_path()
-	hash = get_hash(path)
+	dirname = os.path.dirname(path)
 
-	try:
-		request = requests.get("http://api.thesubdb.com/?action=download&language=en&hash=%s" % (hash), headers={"User-Agent": "SubDB/1.0 (Subs.py; nothing)"})
-		request.raise_for_status()
+	ost = OpenSubtitles() 
+	ost.login('subspy', 'subspy')
 
-	except requests.exceptions.HTTPError:
-		code = request.status_code
-		if (code == 404):
-			print("Subtitles for this file could not be found.")
-			main()
-			return
-		else:
-			print("\nAn HTTP error has occured. Please try again.")
-			main()
-			return
-	
-	except requests.ConnectionError:
-		print("\nA network error has occured. Please try again.")
+	f = File(path)
+
+	data = ost.search_subtitles([{'sublanguageid': 'eng', 'moviehash': f.get_hash(), 'moviebytesize': f.size}])
+
+	id_subtitle_file = data[0].get('IDSubtitleFile')
+	overrides = {id_subtitle_file: Path(os.path.basename(path)).stem + ".srt"}
+
+	status = ost.download_subtitles([id_subtitle_file], override_filenames=overrides, output_directory=dirname, extension='srt')
+
+	if status is None:
+		input("\nSubtitles could not be downloaded.")
+		exit()
+
+	print("\nTitle: " + data[0].get('MovieName') + " (" + data[0].get('MovieYear') + ")")
+	print("Rating: " + data[0].get('MovieImdbRating') + "/10 on IMDb")
+
+	print("\nSubtitles downloaded successfully! Enter [y] for another file.");
+
+	if (str(input()).lower()=="y"):
 		main()
 		return
-	else:
-		ext = os.path.splitext(path)
-		with open("%s.srt" % ext[0], 'w+') as f:
-			f.write(request.text)
-		f.close()
-		print("\nSubtitles downloaded successfully! Enter [y] for another file.");
-		if (str(input()).lower()=="y"):
-			main()
-			return
 
-print(' ___       _                    \n/ __> _ _ | |_  ___    ___  _ _ \n\__ \| | || . \<_-< _ | . \| | |\n<___/`___||___//__/<_>|  _/`_. |\n                      |_|  <___  v0.2 by @sheikhuzairhussain\n')
+print(' ___       _                    \n/ __> _ _ | |_  ___    ___  _ _ \n\__ \| | || . \<_-< _ | . \| | |\n<___/`___||___//__/<_>|  _/`_. |\n                      |_|  <___  v0.5.0 by @sheikhuzairhussain\n')
 main()
